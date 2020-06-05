d59e48c1ff49a9d1bc07f98494e1da25eff53166
brody sullivan <brodysullivan0@gmail.com>
2020-06-05 20:03:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,4 +1,4 @@
-require.pry
+
 def new_hash
 new_hash = {}
 end
@@ -10,37 +10,4 @@ actor = {
 end
 
 def monopoly
-  monopoly =  {
-  :railroads=>
-  {
-    :pieces=>4,
-    :rent_in_dollars=>
-    {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200
-    },
-    :names=>
-    {
-      :reading_railroad=>
-      {
-        "mortgage_value"=>"$100"
-      },
-      :pennsylvania_railroad=>
-      {
-        "mortgage_value"=>"$200"
-      },
-      :b_and_o_railroad=>
-      {
-        "mortgage_value"=>"$400"
-      },
-      :shortline=>
-      {
-        "mortgage_value"=>"$800"
-      }
-    }
-  }
-}
-end
-binding.pry    
+ 
